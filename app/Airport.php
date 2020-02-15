0135d326c15ca41e542209dfdd010054dbf712dd
Bernard Chisumo <bchisumo74@gmail.com>
2020-02-15 22:31:36 +0000

    Created the relation for all models per requirements

--- app/Airport.php
+++ app/Airport.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Airport extends Model
 {
-    //
+    public  function  arrivingFlights()
+    {
+        return $this->hasMany(Flight::class, 'arrivalAirport_id');
+    }
+
+    public  function  departingFlights()
+    {
+        return $this->hasMany(Flight::class, 'departureAirport_id');
+    }
 }
--- app/Flight.php
+++ app/Flight.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    //
+
+    public  function arrivalAirport()
+    {
+        return $this->belongsTo(Airport::class, 'arrivalAirport_id');
+    }
+
+
+    public  function departureAirport()
+    {
+        return $this->belongsTo(Airport::class, 'departureAirport_id');
+    }
+
+    public  function  passengers()
+    {
+        return $this->belongsToMany(Customer::class, 'customer_flight');
+    }
 }
